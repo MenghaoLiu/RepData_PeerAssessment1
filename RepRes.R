86051dd60a91df599e6cb94b09a1e995e4e05f76
MenghaoLiu <menghao1201@pku.edu.cn>
2015-05-15 13:00:04 +0800

    message

--- RepRes.R
+++ RepRes.R
@@ -1,27 +1,27 @@
 
-## Create a new folder named RepRes under your default working directory and set 
-## your workspace into this folder
+# Create a new folder named RepRes under your default working directory and set 
+# your workspace into this folder
 
 if (!file.exists("RepRes")) {
     dir.create("RepRes")
 }
 setwd("RepRes")
 
-## Check whether the required raw dataset has been downloaded. If not, download 
-## the .zip file
+# Check whether the required raw dataset has been downloaded. If not, download 
+# the .zip file
 
 if (!file.exists("data.zip")) {
     url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
     download.file(url, destfile = "data.zip", mode = "wb")
 }
 
-## Unzip the dataset and read the activity.csv file to create myDT dataframe
+# Unzip the dataset and read the activity.csv file to create myDT dataframe
 
 unzip("data.zip")
 myDT <- read.csv("activity.csv", stringsAsFactors = F)
 
-## Transform the format of variable date to class "POSIXct" by  package 
-## lubridate
+# Transform the format of variable date to class "POSIXct" by  package 
+# lubridate
 
 if(!is.element("lubridate", installed.packages()[,1])) {
     print("Installing packages")
@@ -30,24 +30,24 @@ if(!is.element("lubridate", installed.packages()[,1])) {
 library(lubridate)
 myDT$date <- ymd(myDT$date)
 
-## Use aggregate() FUN to collapse the data and get a dataframe named aggDT1  
-## showing the total number of steps taken per day
+# Use aggregate() FUN to collapse the data and get a dataframe named aggDT1  
+# showing the total number of steps taken per day
 
 aggDT1 <- with(myDT, aggregate(steps ~ date, FUN = sum, na.rm = T))
 
-## Make a histogram of the total number of steps taken each day with hist() FUN  
-## in base plotting system
+# Make a histogram of the total number of steps taken each day with hist() FUN  
+# in base plotting system
 
 opar <- par(no.readonly = T)
-par(mar = c(5, 5, 4, 1), cex = .85)
+par(mar = c(5, 5, 4, 1), cex = .85, las = 1)
 with(aggDT1, hist(steps, breaks = 20, col = "steelblue", 
                   xlab = "Total steps", 
-                  main = " Histogram of the total number of steps by day")
+                  main = "Histogram of the total number of steps by day")
 )
 par(opar)
 
-## Build the function stat() which calculates mean and median of a numeric 
-## vector and returns a dataframe, 
+# Build the function stat() which calculates mean and median of a numeric 
+# vector and returns a dataframe
 
 stat <- function(x){
     mean <- mean(x, na.rm = T)
@@ -56,25 +56,25 @@ stat <- function(x){
     return(DT)
 }
 
-## Calculate the mean and median of the total number of steps taken per day
+# Calculate the mean and median of the total number of steps taken per day
 
 stat(aggDT1$steps)
 
-## Create a character vector which formats the interval variable in myDT to 
-## width 4, then construct the variable int_scale showing the decimal hours
+# Create a character vector which formats the interval variable in myDT to 
+# width 4, then construct the variable int_scale showing the decimal hours
 
 int_char <- formatC(myDT$interval, width = 4, flag = "0")
 myDT$int_scale <- as.numeric(substr(int_char, 1, 2)) + 
     as.numeric(substr(int_char, 3, 4)) / 60
 
-## Use aggregate() FUN to collapse the data and get a dataframe named aggDT2  
-## showing the average number of steps taken by 5-minute interval
+# Use aggregate() FUN to collapse the data and get a dataframe named aggDT2  
+# showing the average number of steps taken by 5-minute interval
 
 aggDT2 <- with(myDT, aggregate(steps ~ interval + int_scale, FUN = mean, 
                                na.rm = T))
 
-## Make a time series plot of the 5-minute interval and the average number of   
-## steps taken using base plotting system
+# Make a time series plot of the 5-minute interval and the average number of   
+# steps taken using base plotting system
 
 opar <- par(no.readonly = T)
 par(mar = c(5, 5, 4, 1), cex = .85, las = 1)
@@ -85,16 +85,16 @@ with(aggDT2, plot(int_scale, steps, type = "l",
 )
 par(opar)
 
-## Give the 5-minute interval containing the maximum number of steps
+# Give the 5-minute interval containing the maximum number of steps
 
 subset(aggDT2, aggDT2$steps == max(aggDT2$steps))[, c(1, 3)]
 
-## Calculate the total number of missing values in the dataset 
+# Calculate the total number of missing values in the dataset 
 
 sum(!complete.cases(myDT))
 
-## Fill in all of the missing values with the corresponding average steps of  
-## 5-minute interval to generate a new complete dataframe impDT
+# Fill in all of the missing values with the corresponding average steps of  
+# 5-minute interval to generate a new complete dataframe impDT
 
 int_unique <- unique(myDT$interval)
 impDT <- myDT
@@ -103,19 +103,20 @@ for (i in 1:288) {
                     !complete.cases(impDT)), 1] <- aggDT2$steps[i]
 }
 
-## Check whether the new dataset have incomplete observations or not
+# Check whether the new dataset have incomplete observations or not
 
 sum(!complete.cases(impDT))
 
-## Use aggregate() FUN to collapse the new dataset impDT and get a dataframe  
-## named aggDT3 showing the total number of steps taken per day
+# Use aggregate() FUN to collapse the new dataset impDT and get a dataframe  
+# named aggDT3 showing the total number of steps taken per day
 
 aggDT3 <- with(impDT, aggregate(steps ~ date, FUN = sum))
 
-## Make a histogram of the total number of steps taken each day with new data 
+# Make a histogram of the total number of steps taken each day generated from  
+# NAs filled-in data 
 
 opar <- par(no.readonly = T)
-par(mar = c(5, 5, 4, 1), cex = .85)
+par(mar = c(5, 5, 4, 1), cex = .85, las = 1)
 with(aggDT3, hist(
     steps, breaks = 20, col = "steelblue", 
     xlab = "Total steps",
@@ -123,17 +124,17 @@ with(aggDT3, hist(
 )
 par(opar)
 
-## Calculate the mean and median of the total number of steps taken per day of 
-## the new data
+# Calculate the mean and median of the total number of steps taken per day with 
+# the new data
 
 stat(aggDT3$steps)
 
-## Let's look back to the mean and median of the original data
+# Let's look back to the mean and median of the original data
 
 stat(aggDT1$steps)
 
-## Create a new factor variable weekcat in impDT with two levels -¨C ¡°weekday¡± 
-## and ¡°weekend¡± indicating whether a given date is a weekday or weekend day
+# Create a new factor variable weekcat in impDT with two levels -- ¡°weekday¡± 
+# and ¡°weekend¡± indicating whether a given date is a weekday or weekend day
 
 impDT <- within(impDT, {
     weekcat <- NA
@@ -142,14 +143,14 @@ impDT <- within(impDT, {
 })
 impDT$weekcat <- factor(impDT$weekcat, levels = c("weekend", "weekday"))
 
-## Use aggregate() FUN to collapse impDT and aggDT4 dataset showing the average 
-## number of steps taken by 5-minute interval and weekcat
+# Use aggregate() FUN to collapse impDT and get aggDT4 dataset showing the  
+# average number of steps taken by 5-minute interval and weekcat
 
 aggDT4 <- with(impDT, aggregate(steps ~ int_scale + weekcat, FUN = mean))
 
-## Make a panel plot containing a time series plot of the 5-minute interval and 
-## the average number of steps taken across all weekday days or weekend days, 
-## using lattice plotting system
+# Make a panel plot containing a time series plot of the 5-minute interval and 
+# the average number of steps taken across all weekday days or weekend days, 
+# using lattice plotting system
 
 if(!is.element("lattice", installed.packages()[,1])) {
     print("Installing packages")
